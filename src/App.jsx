9490e4a0e76093d7b91e2aa2a6bc138daaddeea4
nadine k'hila <nadinekhila@gmail.com>
2026-02-15 20:55:46 +0100

    Week 2: First React component with JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from "react";
+const courseTitle = "adv web dev";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const studentName = "Nadine";
+  const student = {
+    name: "Nadine",
+    age: 20,
+    track: "Front-End"
+  };
+  function sayHello() {
+    return "Hello!";
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      
+      <h1>week2</h1>
+      <p>Welcome to {courseTitle}, {studentName}!</p>
+      <label htmlFor="nameInput">Student Name: </label>
+      <input id="nameInput" type="text" />
+      <section>
+        <h3>Student Details:</h3>
+        <p>Name: {student.name}, Age: {student.age}, Track: {student.track}</p>
+      </section>
+      <p>{sayHello()}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
