e1d18c43a6dd65f823093de940e023e807a872dd
nadine k'hila <nadinekhila@gmail.com>
2026-02-15 21:03:22 +0100

    Final: Fixed syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,48 @@
-import * as React from "react";
+import React from "react";
+
+
 const courseTitle = "adv web dev";
 
 function App() {
+  
   const studentName = "Nadine";
+
+  
   const student = {
     name: "Nadine",
     age: 20,
     track: "Front-End"
   };
+
+  
   function sayHello() {
     return "Hello!";
-  };
+  }
+
   return (
     <div>
       
-      <h1>week2</h1>
+      <h1>week 2</h1>
+
+      
       <p>Welcome to {courseTitle}, {studentName}!</p>
+
+      
       <label htmlFor="nameInput">Student Name: </label>
       <input id="nameInput" type="text" />
+
+      
       <section>
         <h3>Student Details:</h3>
         <p>Name: {student.name}, Age: {student.age}, Track: {student.track}</p>
       </section>
+
+      
       <p>{sayHello()}</p>
-      </div>
+    </div>
   );
 }
 
 export default App;
+
+
